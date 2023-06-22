842cd0dfed13279f6d4c1d654311e35d2bce4748
DavidDGamble <daviddgamble@gmail.com>
2023-06-22 12:59:54 -0700

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [toppingCombinations, setToppingCombinations] = useState([]);
+
+  useEffect(() => {
+    fetch('http://files.olo.com/pizzas.json')
+      .then(response => response.json())
+      .then(data => {
+        const combinations = data.reduce((combinations, pizza) => {
+          const toppings = pizza.toppings.sort().join(',');
+          combinations[toppings] = (combinations[toppings] || 0) + 1;
+          return combinations;
+        }, {});
+
+        const sortedCombinations = Object.entries(combinations)
+          .sort((a, b) => b[1] - a[1])
+          .slice(0, 20);
+
+        setToppingCombinations(sortedCombinations);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Top 20 Most Frequently Ordered Pizza Topping Combinations</h1>
+      <ul>
+        {toppingCombinations.map(([toppings, frequency], index) => (
+          <li key={index}>
+            <strong>Rank {index + 1}: </strong>
+            Toppings: {toppings}, Frequency: {frequency}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
